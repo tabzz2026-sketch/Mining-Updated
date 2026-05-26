a7c4d6d0534c4258aa62c15766156216e7f2814a
Shah Areeb <shahareeb@Shahs-MacBook-Pro.local>
2026-05-26 15:31:00 +0530

    upd2

--- .next/dev/static/chunks/src_app_admin_login_page_jsx_0d73h2n._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_04dfxk0._.js"],
-    source: "dynamic"
-});
